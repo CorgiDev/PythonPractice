a26689eaba481756eef94bed99206db020b9fd15
Beth Gray <egray@corgidev.com>
2021-09-26 15:27:51 -0400

    Worked on 2 modules.

--- Learning Python/Ch2/helloworld_start.py
+++ Learning Python/Ch2/helloworld_start.py
@@ -1,3 +1,11 @@
 #
 # Example file for HelloWorld
 #
+
+def main():
+    print("Hello")
+    name = input("What is your name? ")
+    print("It is nice to meet you,",name)
+
+if __name__ == "__main__":
+    main()
--- Learning Python/Ch2/variables_start.py
+++ Learning Python/Ch2/variables_start.py
@@ -1,19 +1,41 @@
-# 
+# ##########################################
 # Example file for variables
-#
+# ##########################################
 
-# Declare a variable and initialize it
+###########################################
+# # Declare a variable and initialize it
 
+# f=0
+# print(f)
 
+###########################################
+# # re-declaring the variable works
 
-# re-declaring the variable works
+# f="abcd"
+# print(f)
 
+###########################################
+# # ERROR: variables of different types cannot be combined
 
+# # print("This is a string " + 123)
 
-# ERROR: variables of different types cannot be combined
+###########################################
+# # ERROR Resolved by converting the int to a string
 
+# print("This is a string " + str(123))
 
-
+###########################################
 # Global vs. local variables in functions
 
+f = 0
 
+def someFunction():
+    global f
+    f="def"
+    print(f)
+
+someFunction()
+print(f)
+
+del f
+print(f)

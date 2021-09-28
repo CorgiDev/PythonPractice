f7307d14d0392a362e44f3f2cf88a8e0efa2eda9
Beth Gray <egray@corgidev.com>
2021-09-28 18:54:04 -0400

    Completed the importing modules lesson.

--- Learning Python/Ch2/modules_start.py
+++ Learning Python/Ch2/modules_start.py
@@ -1,10 +1,14 @@
 # import the math module, which contains features for working with mathematics
-
+import math
 
 # the math module contains lots of pre-built functions
-
+print("The square root of 16 is", math.sqrt(16))
 
 # in addition to functions, some modules contain useful constants
-
+print("Pi is: ", math.pi)
 
 # try some of the math functions for yourself here:
+def square(arg):
+    print("The square root of {} is {}.".format(arg, math.sqrt(arg)))
+
+square(16)

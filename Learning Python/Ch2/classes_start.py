0e90e466a4408b04e6010afa1ebc35468906e2e9
Beth Gray <egray@corgidev.com>
2021-09-28 11:04:34 -0400

    Completed classes module.

--- Learning Python/Ch2/classes_start.py
+++ Learning Python/Ch2/classes_start.py
@@ -2,9 +2,29 @@
 # Example file for working with classes
 #
 
+class myClass():
+    def method1(self):
+        print("myClass method1")
+
+    def method2(self, someString):
+        print("myClass method2 " + someString)
+
+class noClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("noCLass method1")
+
+    def method2(self, someString):
+        print("noclass method2")
 
 def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
 
+    c2=noClass()
+    c2.method1()
+    c2.method2("This is a string.")
 
 if __name__ == "__main__":
     main()

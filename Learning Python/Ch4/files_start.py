a82a9e1ad3907727219f0150cbb8256aeb087b1c
Beth Gray <egray@corgidev.com>
2021-09-30 03:51:53 -0400

    Finished file read/write module.

--- Learning Python/Ch4/files_start.py
+++ Learning Python/Ch4/files_start.py
@@ -4,19 +4,31 @@
 
 def main():  
   # Open a file for writing and create it if it doesn't exist
-
+  # f = open("textfile.txt", "w+")
   
   # Open the file for appending text to the end
+  # f = open("textfile.txt", "a")
 
+  # Open the file for reading
+  f = open("textfile.txt", "r")
 
   # write some lines of data to the file
-
-  
-  # close the file when done
-
+  if (f.mode == 'w+' or f.mode == 'a'):
+    for i in range(10):
+      f.write("This is line " + str(i) + "\r\n")
   
   # Open the file back up and read the contents
+  # if f.mode == 'r':
+  #   contents = f.read()
+  #   print(contents)
 
-    
+  if f.mode == 'r':
+    fl = f.readlines()
+    for x in fl:
+      print (x)
+
+  # close the file when done
+  f.close()
+
 if __name__ == "__main__":
   main()

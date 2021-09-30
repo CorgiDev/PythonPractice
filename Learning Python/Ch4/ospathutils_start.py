d1a079153b93a058b581c91a1f14700991e903f6
Beth Gray <egray@corgidev.com>
2021-09-30 04:40:24 -0400

    Finished os path module.

--- Learning Python/Ch4/ospathutils_start.py
+++ Learning Python/Ch4/ospathutils_start.py
@@ -1,6 +1,7 @@
 #
 # Example file for working with os.path module
 #
+import datetime
 import os
 from os import path
 import datetime
@@ -10,19 +11,28 @@ import time
 
 def main():
   # Print the name of the OS
-
+  # print(os.name)
   
   # Check for item existence and type
+  # print("Item exists: " + str(path.exists("textfile.txt")))
+  # print("Item is a file: " + str(path.isfile("textfile.txt")))
+  # print("Item is a directory: " + str(path.isdir("textfile.txt")))
 
-  
   # Work with file paths
-
+  # print("Item path: " + str(path.realpath("textfile.txt")))
+  # print("Item path: " + str(path.split(path.realpath("textfile.txt"))))
   
   # Get the modification time
-
+  t = time.ctime(path.getmtime("textfile.txt"))
+  print (t)
+  print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
   
   # Calculate how long ago the item was modified
+  tdiff = datetime.datetime.now() - datetime.datetime.fromtimestamp(
+    path.getmtime("textfile.txt")
+  )
 
+  print ("It has been " + str(tdiff) + " since the file was modified.")
   
 if __name__ == "__main__":
   main()

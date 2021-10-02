d4ac400245039f6306d2ffe0e31e034c81c9bc09
Beth Gray <egray@corgidev.com>
2021-10-01 22:45:46 -0400

    Added a start and finish to the matrix practice so I can have a start point to share with others while I work on the finish file.

---
+++ Additional Python Practice/max_sum_matrix start.py
@@ -0,0 +1,12 @@
+# Given a Matrix of numbers, pick one number from each row.
+#
+# 1 2 3 4 9
+# 9 4 5 6 3
+# 3 4 5 6 7
+#
+# If you change columns between rows you must pay a penalty 
+# equivalent to the difference between the 2 column numbers. 
+# abs(x-y)
+# (i.e. if the first number came from column 5 but the second 
+# number came from column 4, you'd take a penalty of 1.)
+# Try to maximize the end total.

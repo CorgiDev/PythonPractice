4af374a674b4faacf230bdcff15db7771bd23129
Beth Gray <egray@corgidev.com>
2021-10-01 20:22:44 -0400

    Edited prac

---
+++ Additional Python Practice/max_sum_matrix.py
@@ -0,0 +1,3 @@
+# Given a Matrix of numbers, pick one number from each row. 
+# If you change columns between rows you must pay a penalty 
+# equivalent to the difference between the 2 column numbers.

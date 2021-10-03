7db6934f385bfdafe831a8b7b0db63100145892b
Beth Gray <egray@corgidev.com>
2021-10-03 02:42:11 -0400

    Got some placeholder code in place on te finished matrix file.

--- Additional Python Practice/max_sum_matrix finish.py
+++ Additional Python Practice/max_sum_matrix finish.py
@@ -1,3 +1,5 @@
+# This is in progress. I have not finished it if this line is still here.
+
 # Given a Matrix of numbers, pick one number from each row.
 #
 # 1 2 3 4 9
@@ -10,3 +12,19 @@
 # (i.e. if the first number came from column 5 but the second 
 # number came from column 4, you'd take a penalty of 1.)
 # Try to maximize the end total.
+
+numberMatrix = [[1,2,3,4,9],[9,4,5,6,3],[3,4,5,6,7]]
+
+maxTotal = 0
+prevCharLoc = 0
+newCharLoc = 0
+penalty = 0
+
+for row in numberMatrix:
+    pass
+    for number in row:
+        pass
+    if newCharLoc != prevCharLoc:
+        prevCharLoc = newCharLoc
+
+print (maxTotal)

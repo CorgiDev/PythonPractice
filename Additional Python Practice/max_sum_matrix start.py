e31fe147aa3c6465c12209b9c373ccc89dce9c39
Beth Gray <egray@corgidev.com>
2021-10-03 05:02:27 -0400

    Cleaned up the prompt text.

--- Additional Python Practice/max_sum_matrix start.py
+++ Additional Python Practice/max_sum_matrix start.py
@@ -1,3 +1,6 @@
+#############################################################################
+# This is in progress. I have not finished it if this line is still here.
+#
 # Given a 2D Array of numbers, pick one number from each row.
 #
 # 1 2 3 4 9
@@ -5,8 +8,15 @@
 # 3 4 5 6 7
 #
 # If you change columns between rows you must pay a penalty 
-# equivalent to the difference between the 2 column numbers. 
+# equivalent to the difference between the 2 column locations 
+# (going 1 - 5, not 0 - 5  for the column numbers for the purpose of this
+# formula).
+#
 # abs(x-y)
-# (i.e. if the first number came from column 5 but the second 
-# number came from column 4, you'd take a penalty of 1.)
+#
+# (i.e. if the first number came from column 5 but the second # number came
+# from column 4, you'd take a penalty of 1.)
 # Try to maximize the end total.
+#############################################################################
+
+numberMatrix = [[1,2,3,4,9],[9,4,5,6,3],[3,4,5,6,7]]
